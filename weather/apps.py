ddb238ee74252eeeaf6a37078eb5836dccb79d39
mawilliams098 <mawilliams098@gmail.com>
2024-03-30 21:49:12 -0400

    Rename weather_app to just weather

--- weather/apps.py
+++ weather/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WeatherAppConfig(AppConfig):
+class WeatherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weather_app'
+    name = 'weather'
