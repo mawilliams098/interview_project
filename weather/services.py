0dc1bbcc99be1d2862322af0818e72abdf4fac25
mawilliams098 <mawilliams098@gmail.com>
2024-03-31 22:57:31 -0400

    Display API call data on front end and validate token.json works

--- weather/services.py
+++ weather/services.py
@@ -3,20 +3,79 @@ import requests
 import json
 import pandas as pd
 import dotenv 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 dotenv.load_dotenv()
 
 OPEN_WEATHER_KEY = os.getenv('OPEN_WEATHER_KEY')
 
-def get_lat_lon(city_name, state_code, country_code, API_key): 
-    r = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+def get_lat_lon(city_name, state_code, country_code): 
+    r = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={OPEN_WEATHER_KEY}').json()
     data = r[0]
     lat, lon = data['lat'], data['lon']
     return lat, lon
 
-def get_current_weather(lat, lon, API_key): 
-    r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=imperial').json()
-    print(r)
+def get_current_weather(lat, lon): 
+    r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPEN_WEATHER_KEY}&units=imperial').json()
+    return r
 
-cville_coords = get_lat_lon("Charlottesville", "VA", "United States", OPEN_WEATHER_KEY)
-cville_weather = get_current_weather(cville_coords[0], cville_coords[1], OPEN_WEATHER_KEY)
-print(cville_weather)
+def get_google_sheet(): 
+    # This function is based off of the Google Workspace python quickstart guide: 
+    # https://developers.google.com/sheets/api/quickstart/python#configure_the_sample
+    
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+    # The ID and range of a sample spreadsheet.
+    SAMPLE_SPREADSHEET_ID = "1_Rxr-2jkJgWmmO6xLJJ61SHEXeRCUVIgv6cXXnvz438"
+    SAMPLE_RANGE_NAME = "Cities"
+
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=8000)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+
+        if not values:
+            print("No data found.")
+            return
+
+        for row in values:
+            print(f"{row[0]}, {row[1]}")
+    except HttpError as err:
+        print(err)
+
+
+get_google_sheet()
--- weather/views.py
+++ weather/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from . import services
 
 def index(request):
-    return render(request, 'weather/index.html')
-
+    lat, lon = services.get_lat_lon("Charlottesville", "VA", "United States")
+    context = services.get_current_weather(lat, lon)
+    print(context)
+    return render(request, 'weather/index.html', context)
